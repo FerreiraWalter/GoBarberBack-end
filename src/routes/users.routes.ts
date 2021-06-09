fae710f7191573dce0cec2d0b268f8ca8a53f8ea
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-06-09 09:16:40 -0300

    Atualizacao do Avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdataUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAthenticated';
 
@@ -34,7 +35,18 @@ usersRouter.patch(
     ensureAuthenticated,
     upload.single('avatar'),
     async (request, response) => {
-    return response.json({ ok: true });
+      try {
+        const updateUserAvatar = new UpdateUserAvatarService();
+
+        const user = await updateUserAvatar.execute({
+          user_id: request.user.id,
+          avatarFilename: request.file.filename
+        });
+
+        return response.json({ ok: true });
+      } catch (err) {
+        return response.status(400).json({ error: err.message })
+      }
   },
 );
 
